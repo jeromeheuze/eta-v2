e7375b9d1100a6016386117a2662b676563aaa39
Jérôme Heuzé <jeromeheuze@gmail.com>
2019-08-20 15:19:18 -0700

    init commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "./includes/global.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Emerging Technology Associates</title>
+    <link href="/css/style.css?v=<?=$version?>" rel="stylesheet" />
+    <link href="/css/bootstrap-grid.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script type="text/javascript">
+</script>
+</body>
+</html>
